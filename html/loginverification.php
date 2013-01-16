04476ea79b0a81bfeb3ed3f0573ec49e64ecfd0d
lthomas93 <lewis.t.thomas@gmail.com>
2013-01-16 14:15:55 -0500

    Update html/loginverification.php
    Added a few more verification for php

--- html/loginverification.php
+++ html/loginverification.php
@@ -1,9 +1,46 @@
 <?php
 
-function verification_username(){
-  if ($username ==""){
-     echo "Please enter a username."
+function verification_username($Username){
+  if ($Username ==""){
+     echo "Please enter a username.</br>"
   }
-  if ((strlen($Username) < 5) || (strlen($Username > 12)) {
-     echo "The username is the wrong length. It must be 5-12 characters.";
+  if ((strlen($Username) < 5) || (strlen($Username) > 12)) {
+     echo "The username is the wrong length. It must be 5-12 characters.</br>";
   }
+}  
+
+function verification_password($Password){
+  if ($Password == ""){
+     echo "Please enter a password</br>.";
+  }
+  if ((strlen($Password) < 6) || (strlen($Password) > 12)){
+     echo "The password is the wrong length. It must be 6-12 characters.</br>";
+  }
+}
+
+function verifcation_lastname($Last_name){
+  if ($Last_name == ""){
+     echo "Please enter your last name. </br>";
+  }
+}
+
+function verification_firstname($First_name){
+  if ($First_name == ""){
+     echo "Please enter your first name. </br>";
+  }
+}
+
+function verficiation_schoolname($School_name){
+  if ($School_name == ""){
+     echo "Please enter your school name. </br>";
+  }
+}
+
+function verification_phonenumber($Phone_number){
+  if ($Phone_number == ""){
+     echo "Please enter a phone number</br>."
+  }
+  if ((strlen($Phone_number) < 10) || (strlen($Phone_number) > 10)){
+     echo "Please verify the ten digit phone number including the area code.</br>"
+  }
+}
