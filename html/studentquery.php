01286f6c16d21f29cac7cab8779c6805149a743d
LT <lewis.t.thomas@gmail.com>
2013-01-28 17:46:54 -0500

    fixed some var prob

--- html/studentquery.php
+++ html/studentquery.php
@@ -5,20 +5,30 @@
 <body>
 <?php
 
-$search = htmlentities($POST["search"]);
-$self = htmlentities($SERVER['PHP_SELF']);
-if ($search != NULL){
+require_once('queryfunction.php');
+//require_once('db_login.php');
+//  $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
+//  if (DB::isError($connection)){
+//     die("Could not query the database: <br />". DB::errorMessage($connection));
+//  }
+
+if(isset($search)){
+$search = htmlentities($_POST["search"]);
+}
+$self = htmlentities($_SERVER['PHP_SELF']);
+if (isset($search)){
+ ($search != NULL);
   query_db($search);
 }
 else {
      echo ('
-     <form action="'.$self'" method="post">
-       <label> Last Name:
+     <form action="'.$self.'" method="post">
+       <label>Last Name:
          <input type="text" name="search"/>
        </label>
-       <input type="submit value="Go!" />
+       <input type="submit" value="Go!" />
      </form>
-     ');
+ ');
 }
 
 ?>
