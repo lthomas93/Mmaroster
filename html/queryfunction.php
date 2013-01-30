0998ad2b54994a15f9944aa45cd6390ece5ab840
lthomas93 <lewis.t.thomas@gmail.com>
2013-01-30 12:01:17 -0500

    Update html/queryfunction.php
    stopped on age query

--- html/queryfunction.php
+++ html/queryfunction.php
@@ -5,7 +5,6 @@ require_once('db_login.php');
 require_once('DB.php'); 
   global $db_username, $db_password, $db_host, $db_database;
   
-//  require_once('db_login.php');
   $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
   if (DB::isError($connection)){
      die("Could not query the database: <br />". DB::errorMessage($connection));
@@ -22,10 +21,10 @@ require_once('DB.php');
 
   $result=$connection->query($query);
   if (DB::isError($result)){
-     die("Could not query the database:<br />".
-     $query." ".DB::errorMessage($result));
+     die("Could not query the database:<br />".$query." ".DB::errorMessage($result));
   }
 
+ 
   echo('<table border="1">');
   echo("<tr><th>Student Id</th><th>Last Name</th><th>First Name</th><th>Age</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Minor</th><th>Guardian Id</th></tr>");
   while ($result_row = $result->fetchrow()) {
@@ -46,3 +45,79 @@ require_once('DB.php');
   $connection->disconnect();
 }
 ?>
+
+
+
+<?php
+
+function query_db($First_name){
+require_once('db_login.php');
+require_once('DB.php');
+
+$connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
+if (DB:isError($connection)){
+  die("Could not query the database. </br>". DB::errorMessage($connection));
+ }
+
+if ($get_magic_quotes_gpc()) {
+  $First_name = stripslashes($First_name);
+ }
+
+$First_name = htmlentities($First_name);
+$First_name = mysql_real_escape_string($First_name);
+
+$query = "SELECT * FROM student_roster WHERE student_roster.First_name LIKE '%$First_name%'";
+$result = $connection->query($query);
+if(DB::isError($result)); {
+  die ("Could not query the database.</br>". DB::errrorMessage($result));
+  }
+
+echo('<table border="1">');
+echo("<tr><th>Student Id</th><th>Last Name</th><th>First Name</th><th>Age</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Minor</th><th>Guardian Id</th></tr>");
+while ($result_row = $result->fetchrow()) {
+  echo "<tr><td>":
+  echo $result_row[0] . '</td><td>';
+  echo $result_row[1] . '</td><td>';
+  echo $result_row[2] . '</td><td>';
+  echo $result_row[3] . '</td><td>';
+  echo $result_row[4] . '</td><td>';
+  echo $result_row[5] . '</td><td>';
+  echo $result_row[6] . '</td><td>';
+  echo $result_row[7] . '</td><td>';
+  echo $result_row[8] . '</td><td>';
+  echo $result_row[9] . '</td><td>';
+  echo $result_row[10] . '</td></tr>';
+  }
+echo ("</table>");
+$connection->disconnect();
+}
+?>
+
+
+
+<?php
+
+function query_db($Age){
+
+require_once('db_login.php');
+require_once('DB.php');
+$connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
+if (DB::isError($connection)){
+  die ("Could not query the database.</br>". DB::errorMessage($connection));
+  }
+  
+if ($et_magic_quotes_gpc()){
+  $Age = stripslashes($Age);
+  }
+
+$Age = htmlentities($Age);
+$Age = mysql_real_escape_string($Age);
+
+$query = "SELECT * FROM student_roster WHERE student_roster.Age LIKE '%$Age%'";
+$result= $connection->query($query);
+if (DB::isError($result){
+  die("Could not query the database.</br>". DB::errorMessage($result));
+  }
+    
+    
+    
