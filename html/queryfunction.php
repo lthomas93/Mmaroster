2a8924f4bc50e6917ce48ea96c18295829105388
lthomas93 <lewis.t.thomas@gmail.com>
2013-01-31 17:00:21 -0500

    Update html/queryfunction.php

--- html/queryfunction.php
+++ html/queryfunction.php
@@ -159,7 +159,7 @@ if ($et_magic_quotes_gpc()){
 $Address = htmlentities($Address);
 $Address = mysql_real_escape_string($Address);
 
-$query = "SELECT * FROM student_roster WHERE student_roster.Age LIKE '%$Age%'";
+$query = "SELECT * FROM student_roster WHERE student_roster.Address LIKE '%$Address%'";
 $result= $connection->query($query);
 if (DB::isError($result){
   die("Could not query the database.</br>". DB::errorMessage($result));
@@ -206,7 +206,7 @@ if ($et_magic_quotes_gpc()){
 $City = htmlentities($City);
 $City = mysql_real_escape_string($City);
 
-$query = "SELECT * FROM student_roster WHERE student_roster.Age LIKE '%$Age%'";
+$query = "SELECT * FROM student_roster WHERE student_roster.City LIKE '%$City%'";
 $result= $connection->query($query);
 if (DB::isError($result){
   die("Could not query the database.</br>". DB::errorMessage($result));
@@ -253,7 +253,7 @@ if ($et_magic_quotes_gpc()){
 $Zip_code = htmlentities($Zip_code);
 $Zip_code = mysql_real_escape_string($Zip_code);
 
-$query = "SELECT * FROM student_roster WHERE student_roster.Age LIKE '%$Age%'";
+$query = "SELECT * FROM student_roster WHERE student_roster.Zip_code LIKE '%$Zip_code%'";
 $result= $connection->query($query);
 if (DB::isError($result){
   die("Could not query the database.</br>". DB::errorMessage($result));
@@ -299,7 +299,7 @@ if ($et_magic_quotes_gpc()){
 $Phone_number = htmlentities($Phone_number);
 $Phone_number = mysql_real_escape_string($Phone_number);
 
-$query = "SELECT * FROM student_roster WHERE student_roster.Age LIKE '%$Age%'";
+$query = "SELECT * FROM student_roster WHERE student_roster.Phone_number LIKE '%$Phone_number%'";
 $result= $connection->query($query);
 if (DB::isError($result){
   die("Could not query the database.</br>". DB::errorMessage($result));
@@ -346,12 +346,58 @@ if ($et_magic_quotes_gpc()){
 $Minor = htmlentities($Minor);
 $Minor = mysql_real_escape_string($Minor);
 
-$query = "SELECT * FROM student_roster WHERE student_roster.Age LIKE '%$Age%'";
+$query = "SELECT * FROM student_roster WHERE student_roster.Minor LIKE '%$Minor%'";
 $result= $connection->query($query);
 if (DB::isError($result){
   die("Could not query the database.</br>". DB::errorMessage($result));
   }
     
+echo('<table border="1">');
+echo("<tr><th>Student Id</th><th>Last Name</th><th>First Name</th><th>Age</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Minor</th><th>Guardian Id</th></tr>");
+while ($result_row = $result->fetchrow()) {
+  echo "<tr><td>":
+  echo $result_row[0] . '</td><td>';
+  echo $result_row[1] . '</td><td>';
+  echo $result_row[2] . '</td><td>';
+  echo $result_row[3] . '</td><td>';
+  echo $result_row[4] . '</td><td>';
+  echo $result_row[5] . '</td><td>';
+  echo $result_row[6] . '</td><td>';
+  echo $result_row[7] . '</td><td>';
+  echo $result_row[8] . '</td><td>';
+  echo $result_row[9] . '</td><td>';
+  echo $result_row[10] . '</td></tr>';
+  }
+echo ("</table>");
+$connection->disconnect();
+}
+?>
+
+
+<?php
+
+function query_db($Guardian_id){
+
+require_once('db_login.php');
+require_once('DB.php');
+$connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
+if (DB::isError($connection)){
+  die ("Could not query the database.</br>". DB::errorMessage($connection));
+  }
+  
+if ($et_magic_quotes_gpc()){
+$Guardian_id = stripslashes($Guardian_id);
+  }
+
+$Guardian_id = htmlentities($Guardian_id);
+$Guardian_id = mysql_real_escape_string($Guardian_id);
+
+$query = "SELECT * FROM student_roster WHERE student_roster.Guardian_id LIKE '%$Guardian_id%'";
+$result= $connection->query($query);
+if (DB::isError($result){
+  die("Could not query the database.</br>". DB::errorMessage($result));
+  }
+
 echo('<table border="1">');
 echo("<tr><th>Student Id</th><th>Last Name</th><th>First Name</th><th>Age</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Minor</th><th>Guardian Id</th></tr>");
 while ($result_row = $result->fetchrow()) {
