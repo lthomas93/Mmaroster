119e6edaecc93a8bd86e9e23557fd540ea0d6763
LT <lewis.t.thomas@gmail.com>
2013-02-11 16:58:35 -0500

    added link to nav menu

--- html/guardianroster.php
+++ html/guardianroster.php
@@ -11,13 +11,10 @@
 </div>
 
 <div id="left">
-  <li><a href="#">Home</a></li></br>
-  <li><a href="studentupload.php">Insert Student</a></li></br>
-  <li><a href="guardianupload.php">Insert Guardian</a></li></br>
-  <li><a href="studentroster.php">Student Roster</a></li></br>
-  <li><a href="studentquery.php">Search Student</a></li></br>
-  <li><a href="guardianroster.php">Guardian Roster</a></li></br>
- <p>1</p>
+<?php
+require_once('nav.php');
+nav_menu();
+?>
 </div>
 
 <div id="right">
