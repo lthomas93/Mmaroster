4be417630e4db7dd2575814336198a4dd3fa0258
LT <lewis.t.thomas@gmail.com>
2013-02-11 17:04:03 -0500

    navigation menu

---
+++ html/nav.php
@@ -0,0 +1,12 @@
+<?php
+function nav_menu(){
+  echo ('
+       <li><a href="#">Home</a></li></br>
+       <li><a href="studentupload.php">Insert Student</a></li></br>
+       <li><a href="guardianupload.php">Insert Guardian</a></li></br>
+       <li><a href="studentroster.php">Student Roster</a></li></br>
+       <li><a href="studentquery.php">Search Student</a></li></br>
+       <li><a href="guardianroster.php">Guardian Roster</a></li></br>
+       ');
+}
+?>
