020b7b11255bf6e67835f403a83d40cf8f9f9b29
LT <lewis.t.thomas@gmail.com>
2013-02-11 16:59:28 -0500

    added link to nav menu

--- html/studentquery.php
+++ html/studentquery.php
@@ -10,12 +10,10 @@
 </div>
 
 <div id="left">
-  <li><a href="#">Home</a></li></br>
-  <li><a href="studentupload.php">Insert Student</a></li></br>
-  <li><a href="guardianupload.php">Insert Guardian</a></li></br>
-  <li><a href="studentroster.php">Student Roster</a></li></br>
-  <li><a href="studentquery.php">Search Students</a></li></br>
- <p>1</p>
+<?php
+require_once('nav.php');
+nav_menu();
+?>
 </div>
 
 <div id="right">
