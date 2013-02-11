e5de60b1c4bdc086f6bd8ee327ba4db78c5d4049
LT <lewis.t.thomas@gmail.com>
2013-02-11 16:05:11 -0500

    fixed some minor syntax errors

--- html/insert2.php
+++ html/insert2.php
@@ -16,7 +16,7 @@ $Address = htmlentities($Address);
 $City = htmlentities($City);
 $State = htmlentities($State);
 $Zip_code = htmlentities($Zip_code);
-$Phone_number = htmlentites($Phone_number);
+$Phone_number = htmlentities($Phone_number);
 
   if(get_magic_quotes_gpc()){
 
@@ -31,7 +31,7 @@ $Zip_code = stripslashes($Zip_code);
 $Phone_number = stripslashes($Phone_number);
   }
 
-$Guardian_id = mysql_real_escape_strip($Guardian_id);
+$Guardian_id = mysql_real_escape_string($Guardian_id);
 $Last_name = mysql_real_escape_string($Last_name);
 $First_name = mysql_real_escape_string($First_name);
 $Age = mysql_real_escape_string($Age);
@@ -39,7 +39,7 @@ $Address = mysql_real_escape_string($Address);
 $City = mysql_real_escape_string($City);
 $State = mysql_real_escape_string($State);
 $Zip_code = mysql_real_escape_string($Zip_code);
-$Phone_number = mysql_real_escape_strip($Phone_number);
+$Phone_number = mysql_real_escape_string($Phone_number);
 
 $query="INSERT INTO guardian_roster VALUES ('$Guardian_id','$Last_name','$First_name','$Age','$Address','$City','$State','$Zip_code','$Phone_number')";
 $result=$connection->query($query);
