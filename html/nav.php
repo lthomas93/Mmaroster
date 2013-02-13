8b5e275cf40db1c294e0a2c19fc505655b7a2759
LT <lewis.t.thomas@gmail.com>
2013-02-13 00:34:05 -0500

    menu

--- html/nav.php
+++ html/nav.php
@@ -5,8 +5,9 @@ function nav_menu(){
        <li><a href="studentupload.php">Insert Student</a></li></br>
        <li><a href="guardianupload.php">Insert Guardian</a></li></br>
        <li><a href="studentroster.php">Student Roster</a></li></br>
-       <li><a href="studentquery.php">Search Student</a></li></br>
+       <li><a href="studentquery.php">Search Students</a></li></br>
        <li><a href="guardianroster.php">Guardian Roster</a></li></br>
+       <li><a href="guardianquery.php">Search Guardians</a></li></br> 
        ');
 }
 ?>
