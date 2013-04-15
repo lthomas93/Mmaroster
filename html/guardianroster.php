64ea61ad989e0fe4790aa90c65169923372193d8
LT <lewis.t.thomas@gmail.com>
2013-04-15 17:39:38 -0400

    cleaned up the code

--- html/guardianroster.php
+++ html/guardianroster.php
@@ -25,30 +25,33 @@ require_once('db_login.php');
 require_once('DB.php');
 
 $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
-  if (DB::isError($connection)){
-    die ("Could not connect to the database!</br>".DB::errorMessage($connection));
-    }
+if (DB::isError($connection))
+{
+	die ("Could not connect to the database!</br>".DB::errorMessage($connection));
+}
 
 $query = "SELECT * FROM guardian_roster";
 $result = $connection->query($query);
-  if (DB::isError($result)){
-    die ("Could not query the database!</br>".DB::errorMessage($result));
-  }
+if (DB::isError($result))
+{
+	die ("Could not query the database!</br>".DB::errorMessage($result));
+}
 
 echo ('<table border="1">');
 echo ('<tr><th>Guardian Id</th><th>Last Name</th><th>First Name</th><th>Age</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th></tr>');
-  while ($result_row = $result->fetchrow()){
-    echo "<tr><td>";
-    echo $result_row[0] . '</td><td>';
-    echo $result_row[1] . '</td><td>';
-    echo $result_row[2] . '</td><td>';
-    echo $result_row[3] . '</td><td>';
-    echo $result_row[4] . '</td><td>';
-    echo $result_row[5] . '</td><td>';
-    echo $result_row[6] . '</td><td>';
-    echo $result_row[7] . '</td><td>';
-    echo $result_row[8] . '</td></tr>';
-  }
+while ($result_row = $result->fetchrow())
+{
+	echo "<tr><td>";
+	echo $result_row[0] . '</td><td>';
+	echo $result_row[1] . '</td><td>';
+	echo $result_row[2] . '</td><td>';
+	echo $result_row[3] . '</td><td>';
+	echo $result_row[4] . '</td><td>';
+	echo $result_row[5] . '</td><td>';
+	echo $result_row[6] . '</td><td>';
+	echo $result_row[7] . '</td><td>';
+	echo $result_row[8] . '</td></tr>';
+}
 echo ("</tabel>");
 $connection->disconnect();
 ?>
