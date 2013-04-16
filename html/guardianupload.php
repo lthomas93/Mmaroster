2d0c28932409be352901999cead5248e12a3e517
LT <lewis.t.thomas@gmail.com>
2013-04-16 10:24:12 -0400

    fixed smalled errors and cleaned up code

--- html/guardianupload.php
+++ html/guardianupload.php
@@ -26,53 +26,65 @@ nav_menu();
 require_once('db_login.php');
 require_once('DB.php');
 $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
-if(DB::isError($connection)){
-die("Could not connect to the database: <br />" .DB::errorMessage($connection));
+if(DB::isError($connection))
+{
+	die("Could not connect to the database: <br />" .DB::errorMessage($connection));
 }
 
 require_once('insert2.php');
 require_once('loginverification.php');
 
-  if (isset($_POST["Guardian_id"])){
-$Guardian_id = $_POST["Guardian_id"];
-  }
+if (isset($_POST["Guardian_id"]))
+{
+	$Guardian_id = $_POST["Guardian_id"];
+}
   
-  if (isset($_POST["Last_name"])){
-$Last_name = $_POST["Last_name"];
-  }
+if (isset($_POST["Last_name"]))
+{
+	$Last_name = $_POST["Last_name"];
+}
 
-  if (isset($_POST["First_name"])){
-$First_name = $_POST["First_name"];
-  }
+if (isset($_POST["First_name"]))
+{
+	$First_name = $_POST["First_name"];
+}
 
-  if (isset($_POST["Age"])){
-$Age = $_POST["Age"];
-  }
+if (isset($_POST["Age"]))
+{
+	$Age = $_POST["Age"];
+}
 
-  if (isset($_POST["Address"])){
-$Address = $_POST["Address"];
-  }
+if (isset($_POST["Address"]))
+{
+	$Address = $_POST["Address"];
+}
 
-  if (isset($_POST["City"])){
-$City = $_POST["City"];
-  }
+if (isset($_POST["City"]))
+{
+	$City = $_POST["City"];
+}
 
-  if (isset($_POST["State"])){
-$State = $_POST["State"];
-  }
+if (isset($_POST["State"]))
+{
+	$State = $_POST["State"];
+}
 
-  if (isset($_POST["Zip_code"])){
-$Zip_code = $_POST["Zip_code"];
-  }
+if (isset($_POST["Zip_code"]))
+{
+	$Zip_code = $_POST["Zip_code"];
+}
   
-  if (isset($_POST["Phone_number"])){
-   $Phone_number = $_POST["Phone_number"];
-  }
+if (isset($_POST["Phone_number"]))
+{
+	$Phone_number = $_POST["Phone_number"];
+}
 
-if (isset($Last_name) && ($First_name) && ($Age) && ($Address) && ($City) && ($State) && ($Zip_code) && ($Phone_number)) {
-  (($Last_name !=NULL) && ($First_name !=NULL) && ($Age !=NULL) && ($Address !=NULL) && ($City !=NULL) && ($State !=NULL) && ($Zip_code !=NULL) && ($Phone_number !=NULL));
-if (($Last_name !=NULL) && ($First_name !=NULL) && ($Age !=NULL) && ($Address !=NULL) && ($City !=NULL) && ($State !=NULL) && ($Zip_code !=NULL) && ($Phone_number !=NULL)){
-register_guardian($Guardian_id,$Last_name,$First_name,$Age,$Address,$City,$State,$Zip_code,$Phone_number);
+if (isset($Last_name) && ($First_name) && ($Age) && ($Address) && ($City) && ($State) && ($Zip_code) && ($Phone_number))
+{
+	(($Last_name !=NULL) && ($First_name !=NULL) && ($Age !=NULL) && ($Address !=NULL) && ($City !=NULL) && ($State !=NULL) && ($Zip_code !=NULL) && ($Phone_number !=NULL));
+	if (($Last_name !=NULL) && ($First_name !=NULL) && ($Age !=NULL) && ($Address !=NULL) && ($City !=NULL) && ($State !=NULL) && ($Zip_code !=NULL) && ($Phone_number !=NULL))
+	{
+		register_guardian($Guardian_id,$Last_name,$First_name,$Age,$Address,$City,$State,$Zip_code,$Phone_number);
         }
 }
 else{
