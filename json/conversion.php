870b8a54b7614d57e5bf4b8f6d640bbe45f76b86
LT <lewis.t.thomas@gmail.com>
2013-04-27 12:28:45 -0400

    test code 1

--- json/conversion.php
+++ json/conversion.php
@@ -1,26 +1,8 @@
 <?php
-if (isset($_POST["author"]))
-{
-	$author = $_POST["author"];
-}
- 
-if (isset($_POST["modified"]))
-{
-	$modified = $_POST["modified"];
-}
 
-if (isset($_POST["removed"]))
+if (isset($_POST["jsondata"]))
 {
-	$removed = $_POST["removed"];
-}
-
-if (isset($_POST["added"]))
-{
-	$added = $_POST["added"]
-}
-
-if (isset($_POST["branch"]))
-{
-	$branch = $_POST["branch"]
+	$jsondata = $_POST["jsondata"];
+	var_dump(json_decode($jsondata));
 }
 ?>
