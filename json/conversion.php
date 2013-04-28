6896959829b6d5c41f8d5b567564a7fb7f6b82fa
LT <lewis.t.thomas@gmail.com>
2013-04-28 18:51:35 -0400

    testing decode

--- json/conversion.php
+++ json/conversion.php
@@ -1,8 +1,9 @@
 <?php
+//if (isset($_POST["jsondata"]))
+//{
+//	$jsondata = $_POST["jsondata"];
 
-if (isset($_POST["jsondata"]))
-{
-	$jsondata = $_POST["jsondata"];
+	$jsondata = '{"a": 1, "b":2}';
 	var_dump(json_decode($jsondata));
-}
+//}
 ?>
