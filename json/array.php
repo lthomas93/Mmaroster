ec1bb921c647b0b9fc2d2405c0c8b5f08a604c46
LT <lewis.t.thomas@gmail.com>
2013-04-29 01:18:36 -0400

    array of variables

---
+++ json/array.php
@@ -0,0 +1,11 @@
+$jsoninfo = array
+(
+	"$authors" => array("username" =>"octokitty");
+	"$modified" => array("modified" => "README.md");
+	"$removed" => array("removed" => "madame-bovary.txt");
+	"$added" => array("added" => "words/madame-bovary.txt");
+	"$branch" => array("repository master_branch" => "master");
+);
+
+echo "$authors";
+?>
