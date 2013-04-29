312442dc150fd470adef10ccbba274b151c5c5b2
LT <lewis.t.thomas@gmail.com>
2013-04-28 20:39:49 -0400

    code used to decode json

--- json/conversion.php
+++ json/conversion.php
@@ -1,12 +1,9 @@
 <?php
-//$jsondata = '{"a": 1, "b":2}';
-$_POST["jsondata"] = "$jsondata";
 if (isset($_POST["jsondata"]))
 {
 	$jsondata = $_POST["jsondata"];
 	if ($jsondata = $_POST["jsondata"])
 	{
-		$jsondata = '{"a": 1, "b":2}';
 		var_dump(json_decode($jsondata, true));
 	}
 }
