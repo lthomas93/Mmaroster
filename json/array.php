0336be6b231e27166e4f67d874250aa0babc5382
LT <lewis.t.thomas@gmail.com>
2013-04-29 01:39:14 -0400

    made function to test in other file

--- json/array.php
+++ json/array.php
@@ -1,11 +1,7 @@
-$jsoninfo = array
-(
-	"$authors" => array("username" =>"octokitty");
-	"$modified" => array("modified" => "README.md");
-	"$removed" => array("removed" => "madame-bovary.txt");
-	"$added" => array("added" => "words/madame-bovary.txt");
-	"$branch" => array("repository master_branch" => "master");
-);
-
-echo "$authors";
+<?php
+function username()
+{
+$decodedJsondata = json_decode($jsondata, true);
+$username = $decodedJsondata['username'];
+}
 ?>
