56cdf233a88009d7dc89fa11b64c14fc7d58628c
LT <lewis.t.thomas@gmail.com>
2013-05-01 12:17:23 -0400

    modified search in jsondata

--- json/array.php
+++ json/array.php
@@ -1,31 +1,57 @@
 <?php
-
 function username()
 {
-global $jsondata;
-$decodedJsondata['username'] = json_decode($jsondata, true);
-$username = $decodedJsondata['username'];
+	global $jsondata;
+	$decodedJsondata['username'] = json_decode($jsondata, true);
+	$username = $decodedJsondata['username'];
 	
-while (list($a, $b) = each($username))
+	while (list($a, $b) = each($username))
+        {
+                while (list($c, $d) = each($b))
+                {
+                        while (list($e, $f) = each($d))
+                        {
+                                if ($e == "committer")
+                                {
+                                        while (list($g, $h) = each($f))
+                                        {
+                                                if ($g =="username")
+                                                {
+                                                        echo "$h <br>";
+                                                }
+                                        }
+                                }
+                        }
+                }
+        }
+}
+
+
+function modified()
+{
+	global $jsondata;
+	$decodedJsondata['modified'] = json_decode($jsondata, true);
+	$modified = $decodedJsondata['modified'];
+		
+	while (list($a, $b) = each($modified))
 	{
 		while (list($c, $d) = each($b))
 		{
-			while (list($e, $f) = each($d))
-			{
-				if ($e == "committer")
+			if ($c = "commits")
+			{	
+				while (list($e, $f) = each($d))
 				{
-					while (list($g, $h) = each($f))
+					if ($e == "modified")
 					{
-						if ($g =="username")
+						while (list($g, $h) = each($f))
 						{
 							echo "$h <br>";
-						}
+						}	
 					}	
 				}	
 			}
-		}
+		}		
 	}
 }
-
 
 ?>
