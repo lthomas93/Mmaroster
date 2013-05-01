e76b9b2a0ed2809c38eaec32f1aca77e6bcf163b
LT <lewis.t.thomas@gmail.com>
2013-05-01 11:51:15 -0400

    username search in jsondata

--- json/array.php
+++ json/array.php
@@ -1,20 +1,31 @@
 <?php
+
 function username()
 {
 global $jsondata;
 $decodedJsondata['username'] = json_decode($jsondata, true);
 $username = $decodedJsondata['username'];
-//print_r($username);
-	while (list($a, $b) = each($username))
+	
+while (list($a, $b) = each($username))
 	{
-	//	echo "$b </br>";
 		while (list($c, $d) = each($b))
 		{
 			while (list($e, $f) = each($d))
 			{
-				echo "$f</br>";
+				if ($e == "committer")
+				{
+					while (list($g, $h) = each($f))
+					{
+						if ($g =="username")
+						{
+							echo "$h <br>";
+						}
+					}	
+				}	
 			}
 		}
 	}
 }
+
+
 ?>
