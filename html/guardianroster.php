c78cd95b8a48bf20e860e2d42102f92cf5bea60f
LT <lewis.t.thomas@gmail.com>
2013-05-05 01:45:16 -0400

    added user log in

--- html/guardianroster.php
+++ html/guardianroster.php
@@ -18,9 +18,10 @@ nav_menu();
 </div>
 
 <div id="right">
-<pre>
+
 
 <?php
+session_start();
 require_once('db_login.php');
 require_once('DB.php');
 
@@ -30,6 +31,12 @@ if (DB::isError($connection))
 	die ("Could not connect to the database!</br>".DB::errorMessage($connection));
 }
 
+if ($_SESSION['loggedin'] != "YES")
+{
+	echo "You are not logged in!";
+	exit;
+}
+
 $query = "SELECT * FROM guardian_roster";
 $result = $connection->query($query);
 if (DB::isError($result))
@@ -37,7 +44,8 @@ if (DB::isError($result))
 	die ("Could not query the database!</br>".DB::errorMessage($result));
 }
 
-echo ('<table border="1">');
+
+echo ('<pre><table border="1">');
 echo ('<tr><th>Guardian Id</th><th>Last Name</th><th>First Name</th><th>Age</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th></tr>');
 while ($result_row = $result->fetchrow())
 {
