8caa6408379776f3dc1b5ef40ca330d2ee43c251
LT <lewis.t.thomas@gmail.com>
2013-05-07 13:12:34 -0400

    cleaned up code and worked some on few other issues

--- html/registration.php
+++ html/registration.php
@@ -17,10 +17,14 @@ nav_menu();
 </div>
 
 <div id="right">
-<pre> 
 <?php
+session_start();
 require_once('db_login.php');
 require_once('DB.php');
+require_once('insert.php');
+require_once('loginverification.php');
+
+
 $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
 	if(DB::isError($connection))
 	{
@@ -28,69 +32,40 @@ $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_databa
   	}
 
 
-	require_once('insert.php');
-	require_once('loginverification.php');
-
-if (isset($_GET["Instructor_id"]))
-{
-	$Instructor_id = $_GET["Instructor_id"];
-}
-
-if (isset($_POST["Username"]))
+if (isset($_POST["Username"]) && ($_POST["Password"]) && ($_POST["Last_name"]) && ($_POST["First_name"]) && ($_POST["School_name"]) && ($_POST["Phone_number"]) && ($_POST["Email"]))
 {
 	$Username = $_POST["Username"];
-	verification_username($Username);
-}
-
-if (isset($_POST["Password"]))
-{
 	$Password = $_POST["Password"];
-	verification_password($Password);
-}
-
-if (isset($_POST["Last_name"]))
-{
 	$Last_name = $_POST["Last_name"];
-	verification_lastname($Last_name);
-}
-
-if (isset($_POST["First_name"]))
-{
 	$First_name = $_POST["First_name"];
+	$School_name=$_POST["School_name"];
+	$Phone_number=$_POST["Phone_number"];
+	$Email=$_POST["Email"];
+
+	$_SESSION['username']=$Username;
+	$_SESSION['password']=$Password;
+	$_SESSION['last_name']=$Last_name;
+	$_SESSION['first_name']=$First_name;
+	$_SESSION['school_name']=$School_name;
+	$_SESSION['phone_number']=$Phone_number;
+	$_SESSION['email']=$Email;
+
+	verification_username($Username);
+	verification_password($Password);
+       	verification_lastname($Last_name);
 	verification_firstname($First_name);
-}
-
-if (isset($_POST["School_name"]))
-{
-	$School_name = $_POST["School_name"];
 	verification_schoolname($School_name);
-}
-
-if (isset($_POST["Phone_number"]))
-{
-	$Phone_number = $_POST["Phone_number"];
 	verification_phonenumber($Phone_number);
-}
-
-if (isset($_POST["Email"]))
-{
-	$Email = $_POST["Email"];
 	verification_email($Email);
+
+	register($Username,$Password,$Last_name,$First_name,$School_name,$Phone_number,$Email); //The insert function
 }
 
-if (isset($Username) && ($Password) && ($Last_name) && ($First_name) && ($School_name) && ($Phone_number) && ($Email))
+//Left alone cause of css
+else
 {
-	(($Username !=NULL) && ($Password !=NULL) && ($Last_name !=NULL) && ($First_name!=NULL) && ($School_name !=NULL) && ($Phone_number !=NULL) && ($Email !=NULL));
-	if (($Username !=NULL) && ($Password !=NULL) && ($Last_name !=NULL) && ($First_name !=NULL) && ($School_name !=NULL) && ($Phone_number != NULL) && ($Email !=NULL))
-	{
-        	register($Username,$Password,$Last_name,$First_name,$School_name,$Phone_number,$Email);
-        }
-}
-
-//Left alone for cause of css
-else{
 echo '
-<h1>Please register!</h1>
+<pre><h1>Please register!</h1>
 <form action="'.$_SERVER["PHP_SELF"].'" method="POST">
 <label> Username:     <input type="text" name="Username" /> </label> </br>
 <label> Password:     <input type="text" name="Password" /> </label> </br>
