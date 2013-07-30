4a752fbf7b218d1d5ebc9737404ab09c19c4093e
LT <lewis.t.thomas@gmail.com>
2013-07-30 14:51:21 -0400

    connected to database

--- html/mysqli/queryfunction.php
+++ html/mysqli/queryfunction.php
@@ -1,4 +1,10 @@
 <?php
 require_once "dblogin.php";
+
+$mysqli= mysqli_connect("$db_host", "$db_username", "$db_password", "$db_database");
+if (mysqli_connect_errno($mysqli))
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>
 
